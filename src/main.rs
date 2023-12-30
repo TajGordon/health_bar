f3a634f5c0bb75d946ca5946510560b15008d6f9
TajGordon <tajgordon@gmail.com>
2023-12-30 20:43:08 +1100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::process;
 
 #[derive(Debug)]
 struct Player {
@@ -33,6 +34,19 @@ struct Weapon {
     atk: i32,
     hp: i32,
     def: i32,
+}
+
+struct Armor {
+    id: String,
+    atk: i32,
+    hp: i32,
+    def: i32,
+}
+
+struct Inventory {
+    player_id: String,
+    weapons: Vec<Weapon>,
+    armor: Vec<Armor>,
 }
 
 #[derive(Debug)]
@@ -89,18 +103,112 @@ impl Unequip for Player {
     }
 }
 
+fn read_user_input(prompt: &str) -> String {
+    let mut input = String::new();
+    println!("{}", prompt);
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line. :(");
+    input.trim().to_ascii_lowercase()
+}
+
+fn equip(inventory: &Inventory, player: &mut Player) {
+    let prompt = read_user_input("What would you like to equip?");
+
+    match prompt.as_str() {
+        "weapon" => equip_weapon(&inventory.weapons, player),
+        "quit" => std::process::exit(1),
+        _ => println!("Valid option not selected, your only option is weapon >:D"),
+    }
+}
+
+fn unequip(player: &mut Player) {
+    loop {
+        let mut type_to_equip = String::new();
+        println!("What would you like to unequip?");
+        println!("Valid options are: Weapon");
+
+        io::stdin()
+            .read_line(&mut type_to_equip)
+            .expect("Failed to read line. :(");
+
+        let type_to_equip = type_to_equip.to_ascii_lowercase();
+        let type_to_equip: &str = type_to_equip.trim();
+
+        match type_to_equip {
+            "weapon" => {let equipped_weapon: Weapon = player.weapon.clone(); player.unequip(&equipped_weapon)},
+            "quit" => std::process::exit(1),
+            _ => println!("Valid option not selected, your only option is weapon >:D"),
+        }
+    }
+}
+
+fn equip_weapon(weapon_inventory: &Vec<Weapon>, player: &mut Player) {
+    if weapon_inventory.len() > 0 {
+        let mut weapon = String::new();
+        println!("Please select an available weapon: ");
+        for item in weapon_inventory {
+            println!("{:?}", item)
+        }
+
+        io::stdin()
+            .read_line(&mut weapon)
+            .expect("Failed to read line. :(");
+
+        let weapon = weapon.to_ascii_lowercase();
+        let weapon: &str = weapon.trim();
+
+        for item in weapon_inventory {
+            if weapon == item.id {
+                let weapon_equipped = player.weapon.clone();
+                player.unequip(&weapon_equipped);
+                player.equip(item);
+            }
+        }
+    } else {println!("No weapons in inventory. :(");}
+}
+
 fn main() {
     println!("Welcome!");
     
-    println!("Please enter a username: ");
+    run_program();
 
-    let mut player_id = String::new();
+    println!("Sayonara :(");
+}
 
-    io::stdin()
-            .read_line(&mut player_id)
-            .expect("Failed to read line.");
+fn init_weapons() {
+    
+    let wooden_stick = Weapon {
+        id: String::from("wooden_stick"),
+        atk: 2,
+        hp: 0,
+        def: 0,
+    };
 
-        let player_id: &str = player_id.trim();
+    let metal_stick = Weapon {
+        id: String::from("metal_stick"),
+        atk: 8,
+        hp: 0,
+        def: 0,
+    };
+
+    let stone_stick = Weapon {
+        id: String::from("stone_stick"),
+        atk: 5,
+        hp: 0,
+        def: 0,
+    };
+
+}
+
+fn get_player_stats(player: &Player) {
+    println!("ID: {}, Lvl: {}, Exp: {}, HP: {}({} Raw), ATK: {}({} Raw), DEF: {}({} Raw), Weapon Equipped: {}, Weapon: {}", &player.id, &player.lvl, &player.exp, player.stat.hp, &player.raw_stat.hp, &player.stat.atk, &player.raw_stat.atk, &player.stat.def, &player.raw_stat.def, &player.weapon_equipped, &player.weapon.id);
+}
+
+fn run_program() {
+    // Init player for now till i figure out how to do this shit :/
+
+    let player_id = read_user_input("Please enter a username: ");
 
     let mut player_stat_raw = PlayerStatRaw {
         hp: 10,
@@ -132,199 +240,53 @@ fn main() {
         default_weapon: fists.clone(),
     };
 
-    let wooden_stick = Weapon {
-        id: String::from("wooden_stick"),
-        atk: 2,
-        hp: 0,
-        def: 0,
-    };
+    let mut weapon_inventory = Vec::new();
+    let mut armor_inventory = Vec::new();
 
-    let metal_stick = Weapon {
-        id: String::from("metal_stick"),
-        atk: 8,
-        hp: 0,
-        def: 0,
-    };
-
-    let stone_stick = Weapon {
-        id: String::from("stone_stick"),
-        atk: 5,
-        hp: 0,
-        def: 0,
-    };
-
-    let mut weapon_inventory: Vec<Weapon> = Vec::new();
     player.equip(&fists);
-
-    weapon_inventory.push(stone_stick);
     weapon_inventory.push(fists);
 
-    
+    let mut player_inventory = Inventory {
+        player_id: player_id.to_string(),
+        weapons: weapon_inventory,
+        armor: armor_inventory,
+    };
+
+    init_weapons();
+
     'menu: loop {
         let mut game_state: GameState = GameState::Menu;
-        
-        let mut command = String::new();
 
         println!("Gamestate: {:?}", game_state);
 
-        println!("Please input a command: ");
+        let command = read_user_input("Please input a command: ");
 
-        io::stdin()
-            .read_line(&mut command)
-            .expect("Failed to read line. :(");
-
-        let command = command.to_ascii_uppercase();
-        let command: &str = command.trim();
-
-        match command {
-            "BATTLE" => {
+        match command.as_str() {
+            "battle" => {
                 game_state = GameState::Battle;
                 println!("Gamestate: {:?}", game_state);
                 'battle: loop {
                     println!("Please input a command: ");
                     
-                    let mut command = String::new();
-
-                    io::stdin()
-                        .read_line(&mut command)
-                        .expect("Failed to read line. :(");
-
-                    let command = command.to_ascii_uppercase();
-                    let command: &str = command.trim();
+                    let mut command = read_user_input("Please input a command: ");
                     
-                    match command {
-                        "QUIT" => break 'menu,
-                        "RETURN" => break 'battle,
-                        "MENU" => break 'battle,
-                        "EQUIP" => {
-                            let mut type_to_equip = String::new();
-                            println!("What would you like to equip?");
-                            println!("Valid options are: Weapon");
-
-                            io::stdin()
-                                .read_line(&mut type_to_equip)
-                                .expect("Failed to read line. :(");
-                        
-                            let type_to_equip = type_to_equip.to_ascii_lowercase();
-                            let type_to_equip: &str = type_to_equip.trim();
-
-                            match type_to_equip {
-                                "weapon" => {
-                                    if weapon_inventory.len() > 0 {
-                                        let mut weapon = String::new();
-                                        println!("Please select an available weapon: ");
-                                        for item in &weapon_inventory {
-                                            println!("{:?}", item)
-                                        }
-                        
-                                        io::stdin()
-                                            .read_line(&mut weapon)
-                                            .expect("Failed to read line. :(");
-                        
-                                        let weapon = weapon.to_ascii_lowercase();
-                                        let weapon: &str = weapon.trim();
-                        
-                                        for item in &weapon_inventory {
-                                            if weapon == item.id {
-                                                let weapon_equipped = player.weapon.clone();
-                                                player.unequip(&weapon_equipped);
-                                                player.equip(item);
-                                            }
-                                        }
-                                    } else {println!("No weapons in inventory. :(");}
-                                },
-                                "quit" => break 'menu,
-                                _ => println!("Invalid option, your only option is weapon >:D"),
-                            };
-                        }
-                        "UNEQUIP" => {
-                            let mut type_to_unequip = String::new();
-                            println!("What would you like to unequip?");
-
-                            io::stdin()
-                                .read_line(&mut type_to_unequip)
-                                .expect("Failed to read line. :(");
-                        
-                            let type_to_unequip = type_to_unequip.to_ascii_lowercase();
-                            let type_to_unequip: &str = type_to_unequip.trim();
-
-                            match type_to_unequip {
-                                "weapon" => {
-                                    let equipped_weapon: Weapon = player.weapon.clone();
-                            player.unequip(&equipped_weapon);
-                                },
-                                _ => println!("Invalid option, your only option is weapon >:D"),
-                            };
-                        },
-                        "PLAYERSTATS" => println!("ID: {}, Lvl: {}, Exp: {}, HP: {}({} Raw), ATK: {}({} Raw), DEF: {}({} Raw), Weapon Equipped: {}, Weapon: {}", player.id, player.lvl, player.exp, player.stat.hp, player.raw_stat.hp, player.stat.atk, player.raw_stat.atk, player.stat.def, player.raw_stat.def, player.weapon_equipped, player.weapon.id),
+                    match command.as_str() {
+                        "quit" => break 'menu,
+                        "return" => break 'battle,
+                        "menu" => break 'battle,
+                        "equip" => {equip(&player_inventory, &mut player); println!("Function was called :)");},
+                        "unequip" => {unequip(&mut player); println!("Function was called :D");},
+                        "playerstats" => get_player_stats(&player),
                         _ => println!("Valid commands are: Quit, Menu, Return, Equip, Unequip"),
                     }
                 }
             }
-            "EQUIP" => {
-                let mut type_to_equip = String::new();
-                println!("What would you like to equip?");
-                println!("Valid options are: Weapon");
-
-                io::stdin()
-                    .read_line(&mut type_to_equip)
-                    .expect("Failed to read line. :(");
-            
-                let type_to_equip = type_to_equip.to_ascii_lowercase();
-                let type_to_equip: &str = type_to_equip.trim();
-
-                match type_to_equip {
-                    "weapon" => {
-                        if weapon_inventory.len() > 0 {
-                            let mut weapon = String::new();
-                            println!("Please select an available weapon: ");
-                            for item in &weapon_inventory {
-                                println!("{:?}", item)
-                            }
-            
-                            io::stdin()
-                                .read_line(&mut weapon)
-                                .expect("Failed to read line. :(");
-            
-                            let weapon = weapon.to_ascii_lowercase();
-                            let weapon: &str = weapon.trim();
-            
-                            for item in &weapon_inventory {
-                                if weapon == item.id {
-                                    let weapon_equipped = player.weapon.clone();
-                                    player.unequip(&weapon_equipped);
-                                    player.equip(item);
-                                }
-                            }
-                        }
-                    }
-                    _ => {println!("Valid option not chosen."); println!("Your only option is weapon! >:)");},
-                }
-            }
-            "UNEQUIP" => {
-                let mut type_to_unequip = String::new();
-                println!("What would you like to unequip?");
-
-                io::stdin()
-                    .read_line(&mut type_to_unequip)
-                    .expect("Failed to read line. :(");
-            
-                let type_to_unequip = type_to_unequip.to_ascii_lowercase();
-                let type_to_unequip: &str = type_to_unequip.trim();
-
-                match type_to_unequip {
-                    "weapon" => {
-                        let equipped_weapon: Weapon = player.weapon.clone();
-                player.unequip(&equipped_weapon);
-                    },
-                    _ => println!("Invalid option, your only option is weapon >:D"),
-                };
-            }
-            "PLAYERSTATS" => println!("ID: {}, Lvl: {}, Exp: {}, HP: {}({} Raw), ATK: {}({} Raw), DEF: {}({} Raw), Weapon Equipped: {}, Weapon: {}", player.id, player.lvl, player.exp, player.stat.hp, player.raw_stat.hp, player.stat.atk, player.raw_stat.atk, player.stat.def, player.raw_stat.def, player.weapon_equipped, player.weapon.id),
-            "QUIT" => break 'menu,
-            "MENU" => println!("You are already in the menu."),
+            "equip" => {equip(&player_inventory, &mut player); println!("Function was called :)");},
+            "unequip" => {unequip(&mut player); println!("Function was called :D");},
+            "playerstats" => get_player_stats(&player),
+            "menu" => println!("You are already in the menu."),
+            "quit" => break 'menu,
             _ => println!("Valid commands are: Battle, Quit, PlayerStats, Equip, Unequip"),
         }
     }
-    println!("Sayonara :(");
 }
